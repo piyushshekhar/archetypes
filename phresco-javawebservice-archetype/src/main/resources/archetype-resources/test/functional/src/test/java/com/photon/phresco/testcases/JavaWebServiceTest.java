cc18a6ba6499d0ffe2ea8d5494243727b2baf296
surendar-r <surendar.rukmangadan@photoninfotech.net>
2013-02-07 16:51:15 +0530

    added to testcases folder in functional

---
+++ phresco-javawebservice-archetype/src/main/resources/archetype-resources/test/functional/src/test/java/com/photon/phresco/testcases/JavaWebServiceTest.java
@@ -0,0 +1,79 @@
+
+package com.photon.phresco.testcases;
+
+import java.io.IOException;
+
+import org.testng.Reporter;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import com.photon.phresco.Screens.WelcomeScreen;
+import com.photon.phresco.uiconstants.PhrescoJavaWebserviceUiConstants;
+import com.photon.phresco.uiconstants.PhrescoUiConstants;
+
+
+
+public class JavaWebServiceTest {
+
+	
+	private  PhrescoUiConstants phrscEnv;
+	private  PhrescoJavaWebserviceUiConstants javaWservice;
+	private  WelcomeScreen welcomeScreen;
+	private  String methodName;
+	
+
+	@Parameters(value = { "browser", "platform" })
+	@BeforeTest
+	public void setUp(String browser,String platform) throws Exception
+	{
+		
+		phrscEnv=new PhrescoUiConstants();
+		javaWservice=new PhrescoJavaWebserviceUiConstants();
+		
+		String selectedBrowser = browser;
+		String selectedPlatform = platform;
+		methodName = Thread.currentThread().getStackTrace()[1]
+		                                					.getMethodName();
+		Reporter.log("Selected Browser to execute testcases--->>"
+		                                					+ selectedBrowser);
+		String applicationURL = phrscEnv.PROTOCOL + "://"+ phrscEnv.HOST + ":" + phrscEnv.PORT+ "/";
+	
+		welcomeScreen = new WelcomeScreen(selectedBrowser,
+				selectedPlatform, applicationURL,
+				phrscEnv.CONTEXT);
+		
+		
+	}
+
+	
+	@Test
+	public void testHelloWorldPage()
+			throws InterruptedException, IOException, Exception {
+		try {
+			
+			System.out
+			.println("---------testHelloWorldPage()-------------");
+			methodName = Thread.currentThread().getStackTrace()[1]
+					.getMethodName();
+			
+			welcomeScreen.javaWebservcieHelloWorld(methodName, javaWservice);
+			
+		} catch (Exception t) {
+			t.printStackTrace();
+
+		}
+	}
+	@AfterTest
+	public  void tearDown() {
+		welcomeScreen.closeBrowser();
+	}
+
+}
+	
+	
+
+
+
+
+
