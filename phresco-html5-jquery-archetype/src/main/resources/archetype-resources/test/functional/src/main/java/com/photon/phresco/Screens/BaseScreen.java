01756244a960420f8e1d939aa521b5cc7b7a7fe3
surendar-r <surendar.rukmangadan@photoninfotech.net>
2013-03-01 13:07:47 +0530

    base screen changes

--- phresco-html5-jquery-archetype/src/main/resources/archetype-resources/test/functional/src/main/java/com/photon/phresco/Screens/BaseScreen.java
+++ phresco-html5-jquery-archetype/src/main/resources/archetype-resources/test/functional/src/main/java/com/photon/phresco/Screens/BaseScreen.java
@@ -174,6 +174,7 @@ public class BaseScreen {
 			// break;
 		}
 		driver = new RemoteWebDriver(server, capabilities);
+		setBrowserResolution();
 		driver.get(applicationURL + applicationContext);
 		// driver.manage().window().maximize();
 		// driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
